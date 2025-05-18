88f08a28c41bdc39bf28ff160302bbac7192d813
mrchuu <chuquyson4@gmail.com>
2025-05-18 08:22:45 +0700

    allow any

--- src/app/component/HomePage/Projects/Preview/index.tsx
+++ src/app/component/HomePage/Projects/Preview/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @next/next/no-img-element */
 import { useEffect, useState } from "react";
 const Preview = ({ project }: any) => {
